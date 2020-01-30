38b0f4c0a618d886801c95e2c02bb259a6df267b
Dmitrii Iurin <dimon07710@yandex.ru>
2020-01-30 23:17:07 +0300

    add users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: %i[password password_confirmation current_password]
+    )
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[username email password password_confirmation])
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  # before_action :authenticate_user!, except: [:show]
+  # before_action :set_current_user, except: [:show]
+  
+  # def show
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+end
