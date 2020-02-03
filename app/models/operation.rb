f7d6642fbf8cabe1ffafe47b1021043b8916ab85
Dmitrii Iurin <dimon07710@yandex.ru>
2020-02-03 23:33:29 +0300

    Operation period check added

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -8,11 +8,21 @@ class Operation < ApplicationRecord
   validates :operation_type, presence: true
   validates :amount, presence: true
 
+  validate :operation_date_should_correspond_to_budget_period
+
   scope :all_by_budget, ->(budget) { where(budget: budget).order(:date) }
   scope :sum_by_type, ->(type) { where(operation_type: type).sum(:amount) }
 
   def type
     # Тип операции может быть :income или :expense
     I18n.t(".operation.#{operation_type}")
+  end
+
+  def operation_date_should_correspond_to_budget_period
+    budget_period = (budget.date_from..budget.date_to)
+
+    return if budget_period.include?(date)
+
+    errors.add(:date, I18n.t('.period_error'))
   end
 end
