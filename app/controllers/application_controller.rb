c3fba6fc2ca011994c94fcb7e1b40f1c44d2ac77
Dmitrii Iurin <dimon07710@yandex.ru>
2020-02-10 22:15:07 +0300

    Changed layout of devise forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :account_update,
-      keys: %i[password password_confirmation current_password]
+      keys: %i[username email password password_confirmation current_password]
     )
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: %i[username email password password_confirmation])
